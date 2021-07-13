ac4ec170d5ef67d1f74ab737c91af9a06f813b52
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-07-13 08:22:16 -0700

    uploaded

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
     first = loadSound("first.mp3");
-    second = loadSound("second.mp3");
+    second = loadSound("2nd.mp3");
 }
 
 function setup() {
@@ -8,8 +13,26 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 600, 400);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.X;
+        leftWristY = results[0].pose.Y;
+
+        rightWristX = results[0].pose.X;
+        rightWristY = results[0].pose.Y;
+    }
 }
