dd9b6bdd17d740099f5843c49032e7d1d9378ce9
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-07-18 19:51:07 +0530

    uploaded

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-song = "";
+song_left = "";
+song_right = "";
+
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
 score_left_wrist = 0;
+score_right_wrist = 0;
 
 function preload() {
     first = loadSound("first.mp3");
@@ -24,13 +27,25 @@ function draw() {
     image(video, 0, 0, 600, 400);
     fill("#ff0000");
     stroke("#ff0000");
-    var something = first.isPlaying()
+
+    var check1 = first.isPlaying();
+    var check2 = second.isPlaying();
+
     if(score_left_wrist > 0.2){
         circle(leftWristX, leftWristY, 20);
         second.stop();
-        if(first.isPlaying() == false){
+        if(check1 == false){
             first.play();
-            document.getElementById("song").innerHTML = "You are not alone song"
+            document.getElementById("song_left").innerHTML = "You are not alone song";
+        }
+    }
+
+    if(score_right_wrist > 0.2){
+        circle(rightWristX, rightWristY, 20);
+        first.stop();
+        if(check2 == false){
+            second.play();
+            document.getElementById("song_left").innerHTML = "Memories song";
         }
     }
 
@@ -45,6 +60,7 @@ function gotPoses(results) {
         console.log(results);
 
         score_left_wrist = results[0].pose.keypoints[9].score;
+        score_right_wrist = results[0].pose.keypoints[9].score;
 
         leftWristX = results[0].pose.X;
         leftWristY = results[0].pose.Y;
